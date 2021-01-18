0bb08364c2f6dc7e13a82827bef399b9a4f0e8b9
connorholm <connorjholm@gmail.com>
2021-01-17 20:44:50 -0600

    Motor turns on for 5 secs

---
+++ moter.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+motorPin = 11     # define the relayPin
+debounceTime = 150
+
+def setup():    
+    GPIO.setmode(GPIO.BOARD)       
+    GPIO.setup(motorPin, GPIO.OUT)   # set relayPin to OUTPUT mode
+
+def runMotor():
+    GPIO.output(motorPin, GPIO.HIGH)
+    time.sleep(5)
+    GPIO.output(motorPin, GPIO.LOW)
+    time.sleep(5)
+
+if __name__ == '__main__':     # Program entrance
+    print ('Program is starting...')
+    setup()
+    try:
+        runMotor()
+        runMotor()
+    except KeyboardInterrupt:   # Press ctrl-c to end the program.
+        destroy()
